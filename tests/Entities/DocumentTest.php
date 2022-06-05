4d6ac6fa17c07a20c25421b39fca26e1b7b280cd
Julius Türich <info@joonlabs.com>
2022-06-06 00:35:52 +0200

    update tests

--- tests/Entities/DocumentTest.php
+++ tests/Entities/DocumentTest.php
@@ -2,6 +2,7 @@
 
 namespace OptiGov\Tests\Entities;
 
+use CMIS\Entities\Document;
 use CMIS\Session\Session;
 use CMIS\Session\SessionFactory;
 use PHPUnit\Framework\TestCase;
@@ -25,17 +26,34 @@ class DocumentTest extends TestCase
      */
     public function testCreateDocument()
     {
-        $response = $this->session
+        $folder = $this->session
+            ->createFolder("Bewohnerparkausweis | Jane Doe", CMIS_OBJECT_TYPE_ID_FOLDER)
+            ->addPostField("objectId", CMIS_REPOSITORY_ID)
+            ->addProperty("repo[304]", "D1") // dienstleistung id
+            ->addProperty("repo[303]", "B2") // buerger id
+            ->addProperty("repo[355]", "A4") // auftrag id
+            ->addProperty("odf:portalname", "optiGov Core")
+            ->addProperty("odf:hersteller", "optiGov")
+            ->addProperty("odf:portalid", "oG-localhost")
+            ->addProperty("odf:queryid", "oG-folder-1")
+            ->addProperty("odf:creationdate", "01.01.2022 08:30")
+            ->addProperty("odf:lastupdate", "01.01.2022 09:45")
+            ->addProperty("odf:authlevel", "hoch")
+            ->addProperty("odf:auftragsid", "1")
+            ->execute();
+
+        $document = $this->session
             ->createDocument(
                 "Bewohnerparkausweis.txt",
                 CMIS_OBJECT_TYPE_ID_DOCUMENT,
                 "Dummy Content")
-            ->addProperty("repo[355]", "A3") // auftrag id
+            ->addPostField("objectId", $folder->getObjectId())
+            ->addProperty("repo[355]", "A4") // auftrag id (antrag id)
             ->execute();
 
-        $this->assertSame(
-            200,
-            $response->getStatusCode()
+        $this->assertInstanceOf(
+            Document::class,
+            $document
         );
     }
 
--- tests/Entities/FolderTest.php
+++ tests/Entities/FolderTest.php
@@ -2,6 +2,7 @@
 
 namespace OptiGov\Tests\Entities;
 
+use CMIS\Entities\Folder;
 use CMIS\Session\Session;
 use CMIS\Session\SessionFactory;
 use PHPUnit\Framework\TestCase;
@@ -25,25 +26,25 @@ class FolderTest extends TestCase
      */
     public function testCreateFolder()
     {
-        $response = $this->session
+        $folder = $this->session
             ->createFolder("Bewohnerparkausweis | Jane Doe", CMIS_OBJECT_TYPE_ID_FOLDER)
-            ->addPostField("objectId", "54c7bf80-f4b2-561a-9656-6ccfdc27ae3d")
+            ->addPostField("objectId", CMIS_REPOSITORY_ID)
             ->addProperty("repo[304]", "D1") // dienstleistung id
             ->addProperty("repo[303]", "B2") // buerger id
-            ->addProperty("repo[355]", "A3") // auftrag id
+            ->addProperty("repo[355]", "A4") // auftrag id
             ->addProperty("odf:portalname", "optiGov Core")
             ->addProperty("odf:hersteller", "optiGov")
             ->addProperty("odf:portalid", "oG-localhost")
             ->addProperty("odf:queryid", "oG-folder-1")
-            ->addProperty("odf:creationdate", "10.01.2022 15:30")
-            ->addProperty("odf:lastupdate", "10.01.2022 15:45")
+            ->addProperty("odf:creationdate", "01.01.2022 08:30")
+            ->addProperty("odf:lastupdate", "01.01.2022 09:45")
             ->addProperty("odf:authlevel", "hoch")
             ->addProperty("odf:auftragsid", "1")
             ->execute();
 
-        $this->assertSame(
-            200,
-            $response->getStatusCode()
+        $this->assertInstanceOf(
+            Folder::class,
+            $folder
         );
     }
 
