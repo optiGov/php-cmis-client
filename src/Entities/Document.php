4b9c8844a43be4c3178dcd763405104faa6b94d4
Julius Türich <info@joonlabs.com>
2022-06-06 00:35:17 +0200

    update entities

--- src/Entities/Document.php
+++ src/Entities/Document.php
@@ -4,5 +4,96 @@ namespace CMIS\Entities;
 
 class Document
 {
+    /**
+     * @var string
+     */
+    public string $objectId;
+
+    /**
+     * @var string
+     */
+    public string $name;
+
+    /**
+     * @var int
+     */
+    public int $creationDate;
+
+    /**
+     * @var string
+     */
+    public string $createdBy;
+
+    /**
+     * @return string
+     */
+    public function getObjectId(): string
+    {
+        return $this->objectId;
+    }
+
+    /**
+     * @param string $objectId
+     * @return Document
+     */
+    public function setObjectId(string $objectId): Document
+    {
+        $this->objectId = $objectId;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Document
+     */
+    public function setName(string $name): Document
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCreationDate(): int
+    {
+        return $this->creationDate;
+    }
+
+    /**
+     * @param int $creationDate
+     * @return Document
+     */
+    public function setCreationDate(int $creationDate): Document
+    {
+        $this->creationDate = $creationDate;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCreatedBy(): string
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * @param string $createdBy
+     * @return Document
+     */
+    public function setCreatedBy(string $createdBy): Document
+    {
+        $this->createdBy = $createdBy;
+        return $this;
+    }
 
 }
--- src/Entities/Folder.php
+++ src/Entities/Folder.php
@@ -4,5 +4,142 @@ namespace CMIS\Entities;
 
 class Folder
 {
+    /**
+     * @var string
+     */
+    public string $objectId;
+
+    /**
+     * @var string
+     */
+    public string $name;
+
+    /**
+     * @var int
+     */
+    public int $creationDate;
+
+    /**
+     * @var string
+     */
+    public string $createdBy;
+
+    /**
+     * @var string
+     */
+    public string $path;
+
+    /**
+     * @var string
+     */
+    public string $parentId;
+
+    /**
+     * @return string
+     */
+    public function getObjectId(): string
+    {
+        return $this->objectId;
+    }
+
+    /**
+     * @param string $objectId
+     * @return Folder
+     */
+    public function setObjectId(string $objectId): Folder
+    {
+        $this->objectId = $objectId;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return Folder
+     */
+    public function setName(string $name): Folder
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCreationDate(): int
+    {
+        return $this->creationDate;
+    }
+
+    /**
+     * @param int $creationDate
+     * @return Folder
+     */
+    public function setCreationDate(int $creationDate): Folder
+    {
+        $this->creationDate = $creationDate;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCreatedBy(): string
+    {
+        return $this->createdBy;
+    }
+
+    /**
+     * @param string $createdBy
+     * @return Folder
+     */
+    public function setCreatedBy(string $createdBy): Folder
+    {
+        $this->createdBy = $createdBy;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPath(): string
+    {
+        return $this->path;
+    }
+
+    /**
+     * @param string $path
+     * @return Folder
+     */
+    public function setPath(string $path): Folder
+    {
+        $this->path = $path;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getParentId(): string
+    {
+        return $this->parentId;
+    }
+
+    /**
+     * @param string $parentId
+     * @return Folder
+     */
+    public function setParentId(string $parentId): Folder
+    {
+        $this->parentId = $parentId;
+        return $this;
+    }
 
 }
